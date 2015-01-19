72169dea5b38482aeaebe2bcd1b57e4ca6c5f82e
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-19 21:37:37 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/blog-manager-light/trunk@1071329 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/otw_blog_manager_help.php
@@ -0,0 +1,3 @@
+<div class="otw_bm_updated">
+	<?php _e( '<p>Want more blog post lists options?<br />Check out the full version of the <a href="http://codecanyon.net/item/blog-manager-for-wordpress/9982292?ref=OTWthemes">Blog Manager for WordPress</a>.</p>', 'otw_bml' );?>
+</div>
