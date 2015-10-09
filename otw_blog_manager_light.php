52aaf776530eab65914e694772ac94957d3276be
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-09 11:40:12 +0000

    version 1.6 release
    git-svn-id: https://plugins.svn.wordpress.org/blog-manager-light/trunk@1262421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- otw_blog_manager_light.php
+++ otw_blog_manager_light.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://OTWthemes.com 
  * Description: Blog Manager for WordPress adds tons of blog functionality to your WordPress based website.
  * Author: OTWthemes.com
- * Version: 1.5
+ * Version: 1.6
  * Author URI: http://themeforest.net/user/OTWthemes
  */
 
